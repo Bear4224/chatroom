f17ed1b566bcc766e5175b768e26e4f2081c9015
Jacob Showman <jacob.showman@mrjaffesclass.com>
2023-02-02 12:39:28 -0800

    Finished step 9

---
+++ src/MessageInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MessageInput() {
+  return (
+    <div className="chat-username">
+      <h4>Message input</h4>
+    </div>
+  );
+}
+
+export default MessageInput;
