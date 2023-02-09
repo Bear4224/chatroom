a6f1e79e298b7353adc824fa95d9cd7ff3ef6253
Jacob Showman <jacob.showman@mrjaffesclass.com>
2023-02-09 13:17:41 -0800

    Added a config to connect to Mr. Jaffe's chat database.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+/*
+//Connect to my Firebase chatroom database.
 const firebaseConfig = {
     apiKey: "AIzaSyDCEof_ybG14erR-ztdh1VH-rHeO43ArCk",
     authDomain: "chatroom-d88f0.firebaseapp.com",
@@ -7,5 +9,16 @@ const firebaseConfig = {
     messagingSenderId: "232277498357",
     appId: "1:232277498357:web:99bd2ba3c5da9348f2a8bc"
   };
+*/
+//Connect to Mr. Jaffe's chatroom database.
+const firebaseConfig = {
+  apiKey: "AIzaSyBwn-ZcL_4N-y3lBtVFJpWomFM7DbvRdXs",
+  authDomain: "chatroom-jaffe.firebaseapp.com",
+  databaseURL: "https://chatroom-jaffe.firebaseio.com",
+  projectId: "chatroom-jaffe",
+  storageBucket: "chatroom-jaffe.appspot.com",
+  messagingSenderId: "423577160816",
+  appId: "1:423577160816:web:2a6af8076c7992cbfd38b5"
+};
 
   export default firebaseConfig;
